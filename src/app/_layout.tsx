ad4cd73c6b2ef8ada34e252852a9647748d65bc0
Kaique <kaiquebezerra30@gmail.com>
2026-07-22 21:24:20 -0300

    feat: initial structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { View } from "react-native";
+import "../styles/global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Slot } from "expo-router";
 
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <View>
+      <View>
+        <Slot />
+      </View>
+    </View>
   );
 }
